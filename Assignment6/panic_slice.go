10a2fc9c942a4c57e002bb4d83565ca327868201
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-23 17:11:40 +0530

    feat: Added question description to both the questions

--- Assignment6/panic_slice.go
+++ Assignment6/panic_slice.go
@@ -1,3 +1,26 @@
+/*
+	1. In the given code, the accessSlice function accepts a slice and index.
+	If the value is present in the slice at that index, the program should print the following.
+
+	"item <index>, value <value at that index in slice>"
+
+	But if the index does not hold any value,
+	it will lead to an index out of range panic in our program.
+
+	Complete the given code to recover from panic inside the accessSlice function, when index out of range panic occurs.
+	Also, Print the following after handling panic
+
+	"internal error: <recovered panic value>"
+
+
+	Example Test Case 1 :
+
+	Input: 3
+	Output:
+	item 3, value 6
+	Testing Panic and Recover
+*/
+
 package main
 
 import (
